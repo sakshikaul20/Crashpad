ff568a2de8850efc1648d0684edad827ee5983f4
sunitathapaaryal <suniaryal2@gmail.com>
2024-04-23 17:57:06 -0600

    update  traveller profile, added  payment and edit profile code

--- frontend/src/components/Traveller/Favourite.tsx
+++ frontend/src/components/Traveller/Favourite.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { Typography } from '@mui/material';
+import { IconButton, Typography } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import crashPadImg1 from '../../images/Lot1.jpg';
+import crashPadImg1 from '../../images/rvpark4.jpeg';
 import crashPadImg2 from '../../images/Lot2.jpg';
 import crashPadImg3 from '../../images/Lot3.jpg';
 import crashPadImg4 from '../../images/Lot4.jpg';
 import crashPadImg5 from '../../images/Lot5.jpg';
+import StarRating from "../Traveller/Rating.tsx";
 
 
 const Favourite: React.FC = () => {
@@ -26,16 +27,17 @@ const Favourite: React.FC = () => {
             />
               <CardContent>
                 <Typography gutterBottom variant="h6" component="div">
-                CrashPad 1
+                Heaven On Earth Moab   
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                City, State, Zip code | Distance
-                Stays | Rating
-                Request | Stays
+                Moab, UT 84055 | 2,600 miles away
+                15-20 September | 
+                $95 per night
+                <StarRating/>
                 </Typography>
             </CardContent>
-        </Card>
-        <Card  sx={{ maxWidth: 200 }}>
+             </Card>
+             <Card  sx={{ maxWidth: 200 }}>
             <CardMedia
                 sx={{ height: 150 }}
                 image={crashPadImg2}
@@ -43,12 +45,13 @@ const Favourite: React.FC = () => {
             />
             <CardContent>
                 <Typography gutterBottom variant="h6" component="div">
-                CrashPad 2
+                Yellowstone Retreat
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                City, State, Zip code | Distance
-                Stays | Rating
-                Request | Stays
+                Idaho, WY, 67095 | 2,800 miles away
+                10-15 July | 4.3
+                $120 per night
+                <StarRating/>
                 </Typography>
             </CardContent>
         </Card>
@@ -60,12 +63,13 @@ const Favourite: React.FC = () => {
             />
             <CardContent>
                 <Typography gutterBottom variant="h6" component="div">
-                CrashPad 3
+                Grand Teton Camp
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                City, State, Zip code | Distance
-                Stays | Rating
-                Request | Stays
+                Moab, UT 84055 | 2,600 miles away
+                15-20 September | 
+                $95 per night
+                <StarRating/>
                 </Typography>
             </CardContent>
         </Card>
@@ -76,13 +80,14 @@ const Favourite: React.FC = () => {
                 title="green iguana"
             />
             <CardContent>
-                <Typography gutterBottom variant="h6" component="div">
-                CrashPad 4
+            <Typography gutterBottom variant="h6" component="div">
+                Grand Teton Camp
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                City, State, Zip code | Distance
-                Stays | Rating
-                Request | Stays
+                Moab, UT 84055 | 2,600 miles away
+                15-20 September | 
+                $95 per night
+                <StarRating/>
                 </Typography>
             </CardContent>
             
@@ -95,12 +100,13 @@ const Favourite: React.FC = () => {
             />
             <CardContent>
                 <Typography gutterBottom variant="h6" component="div">
-                Indiana Dunes State Park beach trail campground
+                Indian Peaks RV Park
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                City, State, Zip code | Distance
-                Stays | Rating
-                Request | Stays
+                Cedar City, UT | 2,600 miles away
+                20 August | 
+                $75 per night
+                <StarRating/>
                 </Typography>
             </CardContent>
         </Card>
